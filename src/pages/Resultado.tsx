039caaf9639758e606769caad8c00a52065be8a8
soutovp <fernandoandradesouto@hotmail.com>
2023-01-08 14:32:01 -0300

    update

--- src/pages/Resultado.tsx
+++ src/pages/Resultado.tsx
@@ -13,9 +13,9 @@ export default function Resultado(){
 
                     <div className="text-center">{imcCalc()}</div>
 
-                    <h3 className="text-[#9627A8] text-[25px] font-[1000] mt-[36px] text-center">Seu gasto calórico é de:</h3>
+                    <h3 className="text-[#9627A8] text-[25px] font-[1000] mt-[36px] text-center">Você precisa consumir:</h3>
                     <div className="mt-[5px] p-[10px] bg-white text-center text-[32px] rounded-[5px] shadow-md">
-                         <p className="text-[#39A827] font-[1000]">{userFullData.gastoCalorico}</p>
+                         <p className="text-[#39A827] font-[1000]">{userFullData.gastoCalorico.toFixed(2)}cal</p>
                     </div>
 
                     <div className="m-auto">
@@ -24,15 +24,15 @@ export default function Resultado(){
                     </div>
                     <h3 className="text-[#9627A8] text-[25px] font-[1000] mt-[10px]">Proteinas -</h3>
                     <div className="mt-[5px] p-[10px] bg-white text-center text-[32px] rounded-[5px] shadow-md">
-                         <p className="text-[#39A827] font-[1000]">{userFullData.gProteina}</p>
+                         <p className="text-[#39A827] font-[1000]">{userFullData.gProteina}g</p>
                     </div>
                     <h3 className="text-[#9627A8] text-[25px] font-[1000] mt-[36px]">Carboidratos -</h3>
                     <div className="mt-[5px] p-[10px] bg-white text-center text-[32px] rounded-[5px] shadow-md">
-                         <p className="text-[#39A827] font-[1000]">{userFullData.gCarboidrato}</p>
+                         <p className="text-[#39A827] font-[1000]">{userFullData.gCarboidrato}g</p>
                     </div>
                     <h3 className="text-[#9627A8] text-[25px] font-[1000] mt-[36px]">Gorduras -</h3>
                     <div className="mt-[5px] p-[10px] bg-white text-center text-[32px] rounded-[5px] shadow-md">
-                         <p className="text-[#39A827] font-[1000]">{userFullData.gGordura}</p>
+                         <p className="text-[#39A827] font-[1000]">{userFullData.gGordura}g</p>
                     </div>
                     <Link to="/" className="mt-[64px] text-center w-[100%] h-[50px] p-2 rounded-[5px] focus:outline-none focus:outline-2 focus:outline-[#9627A8] text-white font-bold text-[20px] bg-[#39A827]">Voltar</Link>
                </form>
